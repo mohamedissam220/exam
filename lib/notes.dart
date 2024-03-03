a7d25e1b6d5df720a876752cd9ee455b0617db29
a107 <mohamedissam220@gmail.com>
2024-03-03 04:51:57 +0200

    finshed all project

---
+++ lib/notes.dart
@@ -0,0 +1,36 @@
+// body: Column(
+//         children: [
+//           Row(children: [
+//             Image(image: AssetImage("assets/images/a.png"),width: 390,height: 120,)
+//           ],),
+//           SizedBox(height: 12,),
+//           Row(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               Text("Workout Programs",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w600),),
+//             ],
+//           ),
+//           SizedBox(height: 10,),
+//
+//           Column(
+//
+//             children: [
+//               CarouselSlider(items: [
+//                 Container(
+//
+//
+//                   padding: EdgeInsets.all(16),
+//                   decoration: BoxDecoration(
+//                       color: Color(0xFFECFDF3),
+//                       image: DecorationImage(image: AssetImage("assets/images/Plank.png",),)
+//                   ),
+//                 ),
+//               ],
+//                   options: CarouselOptions())
+//             ],
+//           ),
+//
+//
+//
+//         ],
+//       ),
